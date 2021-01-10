0aafb1c2252396d205ee438ae0c46790bd940fc4
Balint Kovari <balintmaci@gmail.com>
2021-01-10 03:06:32 +0100

    created binds loader script for elite dangerous

---
+++ elite-dangerous/ed-scripts/binds_loader.js
@@ -0,0 +1,79 @@
+var edBindingsPath = env("LocalAppData") + "\\Frontier Developments\\Elite Dangerous\\Options\\Bindings\\Custom.3.0.binds";
+var edBindings = new ActiveXObject("Microsoft.XMLDOM");
+edBindings.async = false;
+edBindings.load(edBindingsPath);
+
+var edKeyboardLayout = edBindings.getElementsByTagName("KeyboardLayout").item(0).text;
+log(edKeyboardLayout);
+
+function getKeyboardKey(binding) {
+    if (binding) {
+        var device = binding.getAttribute("Device");
+        if (device == "Keyboard") {
+            var key = binding.getAttribute("Key");
+            var modifier = binding.getElementsByTagName("Modifier").item(0);
+            var mod = null;
+            if (modifier) {
+                var temp = getKeyboardKey(modifier);
+                if (temp) {
+                    mod = temp.key
+                }
+            }
+            return {
+                key: key,
+                mod: mod
+            }
+        }
+    }
+    return null;
+}
+
+function processKeyCode(keyString) {
+    if (keyString) {
+        if (stringStartsWith(keyString, "Key_")) {
+            var sliced = keyString.substr(4);
+            if (sliced.length == 1) {
+                return sliced;
+            }
+            return "{" + sliced.toUpperCase() + "}";
+        }
+    }
+    return null;
+}
+
+function mergeModWithKey(keyAndMod) {
+    if (keyAndMod) {
+        if (keyAndMod.key) {
+            var combo = "";
+            if (keyAndMod.mod) {
+                combo = "{" + keyAndMod.mod + "}";
+            }
+            combo = combo + keyAndMod.key;
+            return combo;
+        }
+    }
+    return null;
+}
+
+function getKeyForTask(task) {
+    var task = edBindings.getElementsByTagName(task).item(0);
+    if (task) {
+        var primary = task.getElementsByTagName("Primary").item(0);
+        var keyAndMod = getKeyboardKey(primary);
+        if (keyAndMod == null) {
+            var secondary = getElementsByTagName("Secondary").item(0);
+            keyAndMod = getKeyboardKey(secondary);
+        }
+        if (keyAndMod) {
+            keyAndMod.key = processKeyCode(keyAndMod.key);
+            keyAndMod.mod = processKeyCode(keyAndMod.mod);
+            var combo = mergeModWithKey(keyAndMod);
+            return combo;
+        }
+    }
+    return null;
+}
+
+// log(getKeyForTask("HMDReset"));
+// log(getKeyForTask("ToggleCargoScoop"));
+// log(getKeyForTask("SetSpeed50"));
