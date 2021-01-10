947d08d17c71542e8303a8f849e341ca4440403a
Balint Kovari <balintmaci@gmail.com>
2021-01-10 16:48:49 +0100

    main function supports sending keys a number of times

--- elite-dangerous/ed-scripts/main.js
+++ elite-dangerous/ed-scripts/main.js
@@ -1,7 +1,26 @@
 
-simpleAction = NamedStates.GetNamedStateValue("simpleAction");
+function getAndClear(name) {
+    var value = NamedStates.GetNamedStateValue(name);
+    NamedStates.SetNamedStateValue(name, "");
+    return value;
+}
+
+var simpleAction = getAndClear("simpleAction");
+var actionTimesString = getAndClear("actionTimes");
 log("Target Action: " + simpleAction);
 
-binds = require(mainPath + "binds_loader.js")
-var actionKey = binds.getKeyForTask(simpleAction)
-Application.SendKeys(actionKey);
+var actionTimes = 1
+if (actionTimesString.length > 0) {
+    actionTimes = eval(actionTimesString);
+}
+log("Action Times: " + actionTimes);
+
+binds = require(mainPath + "binds_loader.js");
+
+var actionKey = binds.getKeyForTask(simpleAction);
+log("Action key: " + actionKey);
+for (var i = 0; i < actionTimes; i++) {
+    Application.SendKeys(actionKey);
+    log("Sending " + actionKey);
+    Application.Wait(0.001);
+}
